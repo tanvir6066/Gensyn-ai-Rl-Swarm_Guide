6bee00c93212190e7d913bcd8e8df612cc35cfea
Mayank Gupta 🪿 <91775166+Mayankgg01@users.noreply.github.com>
2025-06-01 02:22:11 +0530

    Create layout.tsx

---
+++ rl-swarm/modal-login/app/layout.tsx
@@ -0,0 +1,35 @@
+import { config } from "@/config";
+import { cookieToInitialState } from "@account-kit/core";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { headers } from "next/headers";
+import "./globals.css";
+import { Providers } from "./providers";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Sign in to Gensyn Testnet",
+  description: "Modal sign in for Gensyn Testnet",
+};
+
+export default async function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  // Persist state across pages
+  const cookieHeaders = await headers();
+  const initialState = cookieToInitialState(
+    config,
+    cookieHeaders.get("cookie") ?? undefined,
+  );
+
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <Providers initialState={initialState}>{children}</Providers>
+      </body>
+    </html>
+  );
+}
